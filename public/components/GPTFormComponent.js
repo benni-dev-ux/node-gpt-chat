12eba6d706a61a0ec21bfd11ecbbeba2b5093687
Benedikt <benediktsteinmueller@googlemail.com>
2023-04-02 19:19:01 +0200

    remove duplicate message

--- public/components/GPTFormComponent.js
+++ public/components/GPTFormComponent.js
@@ -6,9 +6,9 @@ template.innerHTML = `
 </style>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"
 />
-<p class="message response">Hi, I'm a friendly Chatbot. Ask me anything you want to know.</p>
+
 <div class="input-row">
-<input placeholder="Type message..."id="inputField"></input>
+<input  placeholder="Type message..."id="inputField"></input>
 
 <button id="generateRes"><i class="fa-sharp fa-solid fa-paper-plane"></i></button>
 </div>
